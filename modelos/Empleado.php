7d1488a66656895c70632cb037cda79f3906c905
nata100401 <ericanataliaparra@gmail.com>
2019-08-05 15:08:45 -0500

    Initial commit

---
+++ modelos/Empleado.php
@@ -0,0 +1,61 @@
+<?php
+    include ("../config/Conexion.php");
+    class Empleado
+    {
+        public function __construct()
+        {
+
+        }
+
+        public function insertar($Nombre,$Apellido,$Documento,$Telefono,$Rol,$Clave)
+        {
+            $sql="INSERT INTO empleado(Nombre,Apellido,Documento,Telefono,Rol,Clave,Condicion)
+            VALUES ('$Nombre','$Apellido','$Documento','$Telefono','$Rol','$Clave','1')";
+            return ejecutarConsulta($sql);
+        }
+        public function editar($IdEmpleado,$Nombre,$Apellido,$Documento,$Telefono,$Rol,$Clave)
+        {
+            $sql="UPDATE empleado
+            SET Nombre='$Nombre', Apellido='$Apellido', Documento='$Documento', Telefono='$Telefono',  Rol='$Rol', Clave='$Clave'
+            where IdEmpleado='$IdEmpleado'";
+            return ejecutarConsulta($sql);
+        }
+
+        //Desactivar
+        public function desactivar($IdEmpleado)
+        {
+            $sql="UPDATE empleado
+            SET Condicion='0'
+            where IdEmpleado='$IdEmpleado'";
+            return ejecutarConsulta($sql);
+        }
+
+        //Activar
+        public function activar($IdEmpleado)
+        {
+            $sql="UPDATE empleado
+            SET Condicion='1'
+            where IdEmpleado='$IdEmpleado'";
+            return ejecutarConsulta($sql);
+        }
+
+        //mostrar
+        public function mostrar($IdEmpleado)
+        {
+            $sql="SELECT *
+            FROM empleado
+            where IdEmpleado='$IdEmpleado'";
+            return consultarUnaFila($sql);
+        }
+        
+        //listar
+        public function listar()
+        {
+            $sql="SELECT *
+            FROM empleado";
+            
+            return ejecutarconsulta($sql);
+        }
+    } 
+
+?>
